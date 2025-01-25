df898043d1a9839b6a87464c8d25e4898045c680
SundaraRaman R <digital_carver@proton.me>
2025-01-26 02:29:22 +0530

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 /// A Hello, world! program
 
 fn main() {
+    print_hello()
+    print_goodbye()
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, world.");
 }
 
 /// And, we're done! sike, BOO!!!!
