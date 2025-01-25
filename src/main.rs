ce5cccd90eb5afa2b1684b32de7488f0698722c2
SundaraRaman R <digital_carver@proton.me>
2025-01-26 01:22:34 +0530

    only print hello world

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@
 
 fn main() {
     println!("Hello, world!");
-    println!("Oh, you cruel world!");
-    println!("Goodbye, world!");
-    println!("BOO!!!");
 }
 
 /// And, we're done! sike, BOO!!!!
