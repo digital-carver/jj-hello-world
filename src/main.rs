610daa9126ff9586e98acadc8a0bdda569b5d0d2
SundaraRaman R <digital_carver@proton.me>
2025-01-27 03:53:31 +0530

    add a new function

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@
 // The main function runs when our program starts
 fn main() {
     print("Hello, world!");
+    bye();
 }
 
 // save three chars, one of which needs a shift!
 fn print(m: &str) {
     println!("{m}")
+}
+
+fn bye() {
+    print("Goodbye, world!");
 }
 
 /// And, we're done! sike, BOO!!!!
